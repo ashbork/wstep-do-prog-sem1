e89d7b31f6faed1310a5f44a4fb85e0698de395b
maybork <maybork@pm.me>
2021-12-01 13:45:37 +0100

    add additional import example

--- lista7/zad1.py
+++ lista7/zad1.py
@@ -1,9 +1,16 @@
 from moj_modul import Admin as adm
 # from moj_modul import Użytkownik as usr
 
-# dwa inne sposoby importu:
+# trzy inne sposoby importu:
 # import moj_modul
 # from moj_modul import *
+
+# modul = __import__('moj_modul',None, None,[],0) <- to nie jest polecane. Funkcja __import__ pozwala nam zmienić zachowanie
+# przy imporcie. Do importowania po nazwie modułu lepiej i bezpieczniej stosować importlib.import_module(). W liście, która jest czwartym
+# argumentem można podać elementy modułu/podmoduły, które konkretnie należy zimportować. Arg. 2 i 3 determinują kontekst importu.
+# Ten zapis, bez dodatkowych zmian w argumentach, jest równoważny 
+# import moj_modul as modul
+
 
 # alias na całym module:
 # import moj_modul as admin_uzytk
@@ -20,5 +27,6 @@ kowalski = adm("Jan", "Kowalski", 28, "Polska", "Abcdefg", ["addpost", "delpost"
 kowalski.opisz_uzytkownika()
 kowalski.pozdrow_uzytkownika()
 kowalski.wyswietl_przywileje()
+
 # print(dir(adm))
 # print(isinstance(kowalski, usr))
