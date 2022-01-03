7ec63a7e0945ab98f1483038877270f228da012d
maybork <maybork@pm.me>
2022-01-03 23:00:49 +0100

    add comments and better logic

--- lista10/zad2.py
+++ lista10/zad2.py
@@ -1,18 +1,21 @@
-from typing import Type
 import random
+from typing import Union
 import unittest
 
 
 class Employee():
     def __init__(self, first_name: str,
-                 last_name: str, annual_salary: int) -> None:
+                 last_name: str, annual_salary: Union[float, int]) -> None:
+
+        # type validation
         if not isinstance(first_name, str) or not isinstance(first_name, str):
             raise TypeError("name should be of type str")
-        if not isinstance(annual_salary, int):
-            raise TypeError("salary should be of type int")
+        if not isinstance(annual_salary, (int, float)):
+            raise TypeError("salary should be of numeric type")
         self.first_name = first_name
         self.last_name = last_name
-        self.salary = annual_salary
+        # storing salary as a float
+        self.salary = float(annual_salary)
 
     def __repr__(self) -> str:
         return f"Employee({self.first_name} {self.last_name})"
@@ -21,33 +24,39 @@ class Employee():
         return f"Employee: {self.first_name} {self.last_name} with an annual salary of {self.salary}."
 
     def give_raise(self, raise_value=2000) -> None:
-        if not isinstance(raise_value, int):
-            raise TypeError("raise should be of type int")
+        if not isinstance(raise_value, (float, int)):
+            raise TypeError("raise should be of numeric type")
         if raise_value <= 0:
             raise ValueError("raise value should be positive")
-        self.salary += raise_value
+        self.salary += float(raise_value)
 
 
 class EmployeeTestCase(unittest.TestCase):
+
+    # setting up a fixture before each test
     def setUp(self) -> None:
-        self.employee = Employee("Jan", "Kowalski", 120000)
+        self.employee = Employee("Jan", "Kowalski", 120000.0)
 
     def test_default_raise(self):
+        # testing the default raise value
         self.employee.give_raise()
-        self.assertEqual(self.employee.salary, 122000)
+        self.assertEqual(self.employee.salary, 122000.0)
 
     def test_random_raise(self):
-        value = random.randint(1, 100000)
+        # checking a random raise value
+        value = random.random() * 1000
         self.employee.give_raise(value)
-        self.assertEqual(self.employee.salary, 120000 + value)
+        self.assertEqual(self.employee.salary, 120000.0 + value)
 
     def test_bad_raise_value(self):
+        # checking for negative raise value
         with self.assertRaises(ValueError):
-            self.employee.give_raise(-100)
+            self.employee.give_raise(-100.0)
 
     def test_bad_raise_type(self):
+        # checking for wrong raise type
         with self.assertRaises(TypeError):
-            self.employee.give_raise(0.5)
+            self.employee.give_raise("10")
 
 
 if __name__ == '__main__':
