6e536e597c07bdb93157d0c5a411aa50bcc991bb
maybork <maybork@pm.me>
2022-01-06 13:35:29 +0100

    add documentation

--- lista11/zad2/database_creation.py
+++ lista11/zad2/database_creation.py
@@ -1,69 +1,91 @@
 import pathlib
 import random
 import sys
+from typing import Iterable
 
 import lorem
 import names
 
 import database as db
 
+# constants, used to configure the newly created db
 USERS_TO_CREATE = 20
 POSTS_TO_CREATE = 100
-DB_PATH = "/home/ashley/wstep-do-prog-sem1/lista11/zad2/mydb.sqlite"
+DB_PATH = pathlib.Path(
+    "/home/ashley/wstep-do-prog-sem1/lista11/zad2/mydb.sqlite")
+
+# generator, returns an iterable of strings that can be later used
+# to populate the db with users
 
 
-def user_generator(count):
+def user_generator(count: int) -> Iterable[str]:
     for i in range(count):
+        # get a random first name
         name = names.get_first_name()
+        # get a random age
         age = random.randint(16, 90)
+        # yield the result, we also need the lines to end with
+        # , if the user isn't the last one within the query
+        # and a ; if it is.
         yield f"\t('{name}', {age}){',' if i != count-1 else ';'} \n"
 
 
-def post_generator(usercount, count):
+# generator, returns an iterable of strings that can be later used
+# to populate the db with posts
+def post_generator(usercount: int, count: int) -> Iterable[str]:
     for i in range(count):
+        # get a random 'lorem ipsum' style sentence
         content = lorem.sentence()
+        # get a random user ID, assigning the post to an existing user
         uid = random.randint(1, usercount)
+        # yield the result, we also need the lines to end with
+        # , if the post isn't the last one within the query
+        # and a ; if it is.
         yield f"\t('{content}', {uid}){',' if i != count-1 else ';'} \n"
 
 
-if pathlib.Path(DB_PATH).exists():
+# check for db's existence, quit if it's already there
+if DB_PATH.exists():
     print("db already exists, exiting")
     sys.exit()
 
-db = db.Database(DB_PATH)
+with db.Database(DB_PATH) as db:
 
-db.run_query("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL
-);
-""")
+    # creates a users table
+    db.run_query("""
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER NOT NULL
+    );
+    """)
 
-db.run_query("""
-CREATE TABLE IF NOT EXISTS posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (id)
-        REFERENCES użytkownicy (user_id)
-            ON UPDATE RESTRICT
-            ON DELETE RESTRICT
-)
-""")
+    # creates a posts table
+    db.run_query("""
+    CREATE TABLE posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (id)
+            REFERENCES użytkownicy (user_id)
+                ON UPDATE RESTRICT
+                ON DELETE RESTRICT
+    )
+    """)
+    # constructs a query that will insert random users into the table
+    users_query = """INSERT INTO
+        users (name, age)
+    VALUES\n"""
+    for x in user_generator(USERS_TO_CREATE):
+        users_query += x
 
-users_query = """INSERT INTO
-    users (name, age)
-VALUES\n"""
-for x in user_generator(USERS_TO_CREATE):
-    users_query += x
+    db.run_query(users_query)
 
-db.run_query(users_query)
+    # constructs a query that will insert random posts into the table
+    posts_query = """INSERT INTO
+        posts (content, user_id)
+    VALUES\n"""
+    for x in post_generator(USERS_TO_CREATE, POSTS_TO_CREATE):
+        posts_query += x
 
-posts_query = """INSERT INTO
-    posts (content, user_id)
-VALUES\n"""
-for x in post_generator(USERS_TO_CREATE, POSTS_TO_CREATE):
-    posts_query += x
-
-db.run_query(posts_query)
+    db.run_query(posts_query)
--- lista11/zad2/queries.py
+++ lista11/zad2/queries.py
@@ -5,40 +5,39 @@ import database as db
 
 DB_PATH = "/home/ashley/wstep-do-prog-sem1/lista11/zad2/mydb.sqlite"
 
-db = db.Database(DB_PATH)
 
-get_all_users = """
-SELECT name
-FROM users;
-"""
+with db.Database(DB_PATH) as db:
+    get_all_users = """
+    SELECT name
+    FROM users;
+    """
 
-users = db.run_read_query(get_all_users)
-if not users:
-    raise DatabaseError("database should contain users")
+    users = db.run_read_query(get_all_users)
+    if not users:
+        raise DatabaseError("database should contain users")
 
-randuser = users[random.randint(1, len(users))][0]
+    randuser = users[random.randint(1, len(users))][0]
 
+    get_posts_from_random_user = f"""
+    SELECT posts.id, posts.content
+    FROM posts, users
+    WHERE posts.user_id = users.id AND users.name IS "{randuser}"
+    """
 
-get_posts_from_random_user = f"""
-SELECT posts.id, posts.content
-FROM posts, users
-WHERE posts.user_id = users.id AND users.name IS "{randuser}"
-"""
+    randuser_posts = db.run_read_query(get_posts_from_random_user)
+    if not randuser_posts:
+        print("No posts by this person")
+    else:
+        print(f"Posts by: {randuser}")
+        for x in randuser_posts:
+            print(x)
 
-randuser_posts = db.run_read_query(get_posts_from_random_user)
-if not randuser_posts:
-    print("No posts by this person")
-else:
-    print(f"Posts by: {randuser}")
-    for x in randuser_posts:
-        print(x)
-
-get_postcount_from_random_user = f"""
-SELECT COUNT(*), users.name
-FROM posts, users
-WHERE posts.user_id = users.id AND users.name IS "{randuser}"
-"""
-postcount = db.run_read_query(get_postcount_from_random_user)
-if postcount:
-    count, name = postcount[0]
-    print(f"\n{name} has made {count} posts.")
+    get_postcount_from_random_user = f"""
+    SELECT COUNT(*), users.name
+    FROM posts, users
+    WHERE posts.user_id = users.id AND users.name IS "{randuser}"
+    """
+    postcount = db.run_read_query(get_postcount_from_random_user)
+    if postcount:
+        count, name = postcount[0]
+        print(f"\n{name} has made {count} posts.")
