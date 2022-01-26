99f68788f037e27ada410a2377cfdb5598bda544
maybork <maybork@pm.me>
2022-01-26 13:04:19 +0100

    add more input validation and a clarifying comment

--- lista13/zad1.py
+++ lista13/zad1.py
@@ -5,8 +5,11 @@ def format_bytes(number: int) -> str:
     if number > (1024 ** 9):
         raise ValueError(
             f"can't handle more than 1023 yobibytes (got {number})")
+    if number < 0:
+        raise ValueError(f"expected positive integer (got {number})")
     units = ("bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
     results = []
+    # for each unit, until our number isn't zero
     for unit in units:
         if number == 0:
             break
