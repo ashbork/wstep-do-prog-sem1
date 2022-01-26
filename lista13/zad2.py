e27fee71e878e43f3511b3083156613e37c158ae
maybork <maybork@pm.me>
2022-01-26 12:58:47 +0100

    add example with list comprehension

--- lista13/zad2.py
+++ lista13/zad2.py
@@ -4,3 +4,6 @@ result = []
 for num in filter(divby_three_or_five, range(50)):
     result.append(num)
 print(result)
+
+# alternatywnie (zwięźlej i czytelniej):
+# [num for num in range(50) if num % 3 == 0 and num % 5 == 0]
