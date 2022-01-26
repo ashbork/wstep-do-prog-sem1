e90d408b4e81b99e125f6352937fe8baaf5598da
maybork <maybork@pm.me>
2022-01-26 12:59:22 +0100

    modify if checks to be more explicit

--- lista13/zad1.py
+++ lista13/zad1.py
@@ -1,21 +1,20 @@
 
 def format_bytes(number: int) -> str:
     if not isinstance(number, int):
-        raise TypeError(f"expected integer")
+        raise TypeError(f"expected integer (got {type(number)})")
     if number > (1024 ** 9):
         raise ValueError(
             f"can't handle more than 1023 yobibytes (got {number})")
     units = ("bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
     results = []
     for unit in units:
-        if not number:
+        if number == 0:
             break
         remainder = number % 1024
-        if remainder:
-            results.append(f'{remainder} {unit} + ')
+        if remainder != 0:
+            results.append(f'{remainder} {unit}, ')
         number = number // 1024
-        print(remainder, number, unit)
-    return "".join(reversed(results)).strip("+ ")
+    return "".join(reversed(results)).strip(", ")
 
 
-print(format_bytes(1208925819614629174706176123))
+print(format_bytes(1025))
