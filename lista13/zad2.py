d19df8b0484907c380166d6988fe0818de23a1f5
maybork <maybork@pm.me>
2022-01-26 15:36:59 +0100

    pep8 compliance

--- lista13/zad2.py
+++ lista13/zad2.py
@@ -1,4 +1,4 @@
-divby_three_or_five = lambda a : True if (a % 3 == 0 or a % 5 == 0) and a != 0 else False
+divby_three_or_five = lambda a: True if (a % 3 == 0 or a % 5 == 0) and a != 0 else False
 
 result = []
 for num in filter(divby_three_or_five, range(50)):
